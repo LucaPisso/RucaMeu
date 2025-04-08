354e957679633f63ff441767768d99f7f52c7c7a
Nicolas Solans <nico.solans.drc@gmail.com>
2025-04-08 10:22:11 -0300

    inputs name, lastName y phone con sus handlers y validaciones

--- RucaMeu/src/components/Register.jsx
+++ RucaMeu/src/components/Register.jsx
@@ -1,12 +1,54 @@
 import React, { useState } from "react";
 
 const Register = () => {
+  //ESTADOS
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [button, setButton] = useState(false);
+
+  //REGEX
+  const phoneRegex = /^[0-9]{9,12}$/;
   const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+
+  //ESTADOS DE VALIDACIONES
+  const [nameValido, setNameValido] = useState(false);
+  const [lastNameValido, setlastNameValido] = useState(false);
+  const [phoneValido, setPhoneValido] = useState(false);
   const [emailValido, setEmailValido] = useState(false);
   const [passwordValido, setPasswordValido] = useState(false);
+
+  //HANDLERS
+
+  function nameHandler(event) {
+    setName(event.target.value);
+    if (name === "") {
+      setNameValido(false);
+    } else {
+      setNameValido(true);
+    }
+  }
+
+  function lastNameHandler(event) {
+    setLastName(event.target.value);
+    setName(event.target.value);
+    if (!lastName) {
+      setlastNameValido(false);
+    } else {
+      setlastNameValido(true);
+    }
+  }
+
+  function phoneHandler(event) {
+    setPhone(event.target.value);
+    if (!phoneRegex.test(phone)) {
+      setPhoneValido(false);
+    } else {
+      setPhoneValido(true);
+    }
+  }
 
   function emailHandler(event) {
     setEmail(event.target.value);
@@ -20,6 +62,8 @@ const Register = () => {
   function passwordHandler(event) {
     setPassword(event.target.value);
     if (password === "") {
+      setPasswordValido(false);
+    } else if (password.length < 8) {
       setPasswordValido(false);
     } else {
       setPasswordValido(true);
@@ -38,7 +82,13 @@ const Register = () => {
     setEmail("");
     */
     event.preventDefault();
-    if (emailValido && passwordValido) {
+    if (
+      emailValido &&
+      passwordValido &&
+      phoneValido &&
+      nameValido &&
+      lastNameValido
+    ) {
       console.log("Se envió la info");
       //formulario.submit()
     } else {
@@ -46,12 +96,18 @@ const Register = () => {
     }
   }
 
-  console.log("email");
-  console.log(email);
-  console.log("validacion");
+  console.log("validacion name");
+  console.log(nameValido);
+
+  console.log("validacion lastName");
+  console.log(lastNameValido);
+
+  console.log("validacion phone");
+  console.log(phoneValido);
+
+  console.log("validacion email");
   console.log(emailValido);
-  console.log("contraseña");
-  console.log(password);
+
   console.log("validacion password");
   console.log(passwordValido);
 
@@ -61,10 +117,24 @@ const Register = () => {
   return (
     <div>
       <form action="POST">
-        <label htmlFor="mail">E-mail:</label>
+        <label htmlFor="name">Nombre:</label>
         <br />
-        <input type="email" name="mail" onChange={emailHandler} />
-        {}
+        <input type="text" name="name" onChange={nameHandler} />
+        <br />
+        <br />
+        <label htmlFor="lastName">Apellido:</label>
+        <br />
+        <input type="text" name="lastName" onChange={lastNameHandler} />
+        <br />
+        <br />
+        <label htmlFor="phone">Teléfono:</label>
+        <br />
+        <input type="text" name="phone" onChange={phoneHandler} />
+        <br />
+        <br />
+        <label htmlFor="email">E-mail:</label>
+        <br />
+        <input type="email" name="email" onChange={emailHandler} />
         <br />
         <br />
         <label htmlFor="password">Contraseña:</label>
