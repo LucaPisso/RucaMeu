d48da08b7730c669887796338a3f60dbec416bff
Nicolas Solans <nico.solans.drc@gmail.com>
2025-05-27 17:30:02 -0300

    react-router-dom

--- Front/src/components/Register.jsx
+++ Front/src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 const Register = ({ submit, errors, refs }) => {
   const [formData, setFormData] = useState({
