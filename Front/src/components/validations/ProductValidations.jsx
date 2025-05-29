6b5a19469c27f9d94f76cced6a6f22497c644798
Lucas Luppi <lucasgluppi@gmail.com>
2025-05-29 11:13:13 -0300

    Corrección validaciones productos

--- Front/src/components/validations/ProductValidations.jsx
+++ Front/src/components/validations/ProductValidations.jsx
@@ -1,28 +1,34 @@
-//Corregir focus a como estaba antes. Así no es funcional
-
 const ProductValidations = ({ datos, refs }) => {
   const errors = {}; //para manejar errores al enviar formulario. Si fuese en tiempo real se necesita useState
   const { nameRef, descriptionRef, categoryRef, priceRef, stockRef } = refs;
 
   if (!datos.name?.trim()) {
     errors.name = "El nombre es obligatorio";
-    nameRef.current.focus();
   }
 
   if (!datos.category?.trim()) {
     errors.category = "La categoría es obligatoria";
-    categoryRef.current.focus();
   }
 
   const price = parseFloat(datos.price);
   if (isNaN(price) || price < 0) {
     errors.price = "El precio debe ser un número válido y no negativo";
-    priceRef.current.focus();
   }
 
   const stock = parseInt(datos.stock);
   if (isNaN(stock) || stock < 0) {
     errors.stock = "El stock debe ser un número válido y no negativo";
+  }
+
+  if (errors.name && nameRef.current) {
+    nameRef.current.focus();
+  } else if (errors.description && descriptionRef.current) {
+    descriptionRef.current.focus();
+  } else if (errors.category && categoryRef.current) {
+    categoryRef.current.focus();
+  } else if (errors.price && priceRef.current) {
+    priceRef.current.focus();
+  } else if (errors.stock && stockRef.current) {
     stockRef.current.focus();
   }
 
