085d11d3137263a30944fe7a669f29e06c4be646
Nicolas Solans <nico.solans.drc@gmail.com>
2025-11-11 19:51:59 -0300

    Items del carrito en CarritoPage

--- Front/src/pages/CarritoPage.jsx
+++ Front/src/pages/CarritoPage.jsx
@@ -1,15 +1,21 @@
 import Card from "../components/Card";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import CardItemCart from "../components/CardItemCart";
 
 const CarritoPage = () => {
   const [carrito, setCarrito] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const token = localStorage.getItem("RucaMeu-token");
   const API_URL = import.meta.env.VITE_API_BASE_URL;
   useEffect(() => {
     const fetchCarrito = async () => {
       try {
+        if (!token) {
+          toast.error("Debes iniciar sesión para ver el carrito");
+          navigate("/login");
+        }
         const res = await fetch(`${API_URL}/GetCartByToken`, {
           method: "GET",
           headers: {
@@ -20,8 +26,9 @@ const CarritoPage = () => {
 
         if (!res.ok) throw new Error("Falló al obtener productos");
         const response = await res.json();
-        setCarrito(response);
-        console.log(response);
+        setCarrito(response.items);
+        setTotalPrice(response.totalPrice);
+        console.log(response.items);
       } catch (err) {
         console.error(err);
         toast.error("Error: " + err.message);
@@ -29,36 +36,21 @@ const CarritoPage = () => {
     };
 
     fetchCarrito();
-  }, []);
+  }, [token, API_URL]);
 
   return (
     <>
       <h1>Mi carrito</h1>
       <div className="card-container">
+        <h2>Total: ${totalPrice}</h2>
         {carrito.length > 0 ? (
-          carrito.map((c) => <Card key={c.id} carrito={c} />)
+          carrito.map((item) => (
+            <CardItemCart key={item.productDTO.id} item={item} />
+          ))
         ) : (
           <h1>No hay productos en el carrito.</h1>
         )}
       </div>
-
-      {/* const filteredBooks = books
-            .filter(product => search ? (book.title.toLowerCase().includes(search.toLowerCase()) || book.author.toLowerCase().includes(search.toLowerCase())) : book)
-            .map(((product) => (
-              <BookItem
-                key={book.id}
-                id={book.id}
-                title={book.title}
-                author={book.author}
-                rating={book.rating}
-                pages={book.pageCount}
-                imageUrl={book.imageUrl}
-                summary={book.summary}
-                available={book.available}
-                onDeleteBook={handleDeleteBook}
-              />
-            )));
-            </> */}
     </>
   );
 };
